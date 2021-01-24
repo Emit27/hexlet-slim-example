c8834f9e11061910e0814b9f958f5935e6dd45cc
Dmitryi_Ignatev <46993682+Emit27@users.noreply.github.com>
2021-01-24 18:55:00 +1000

    change

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function ($request, $response) {
+    $response->getBody()->write('Welcome to Slim!');
+    return $response;
+    // Благодаря пакету slim/http этот же код можно записать короче
+    // return $response->write('Welcome to Slim!');
+});
+$app->run();
