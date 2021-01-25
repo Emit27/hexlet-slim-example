d7c07946ebea65f4c5c22c7bb377eb86610f6fc6
Dmitryi_Ignatev <46993682+Emit27@users.noreply.github.com>
2021-01-25 18:13:45 +1000

    change

--- public/index.php
+++ public/index.php
@@ -1,16 +1,41 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+namespace SHR;
 
 use Slim\Factory\AppFactory;
+use Faker\Factory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$faker = Factory::create();
+$faker->seed(1234);
+
+$domains = [];
+for ($i = 0; $i < 10; $i++) {
+    $domains[] = $faker->domainName;
+}
+
+$phones = [];
+for ($i = 0; $i < 10; $i++) {
+    $phones[] = $faker->phoneNumber;
+}
 
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
+    return $response->write('go to the /phones or /domains');
 });
+
+// BEGIN (write your solution here)
+$app->get('/phones', function ($request, $response) use ($phones) {
+
+    return $response->write(json_encode($phones));
+});
+$app->get('/domains', function ($request, $response) use ($domains) {
+
+    return $response->write(json_encode($domains));
+});
+// END
+
 $app->run();
