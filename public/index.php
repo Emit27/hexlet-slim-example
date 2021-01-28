7d8b0c3d0bc6532df653632f541cb51ba926ca43
Dmitryi_Ignatev <46993682+Emit27@users.noreply.github.com>
2021-01-28 17:04:37 +1000

    render

--- public/index.php
+++ public/index.php
@@ -3,12 +3,16 @@
 namespace SHR;
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$companies = \SHR\Gen\Generator::generate(100);
-
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 
@@ -16,23 +20,12 @@ $app->get('/', function ($request, $response, $args) {
     return $response->write('open something like (you can change id): /companies/5');
 });
 
-// BEGIN (write your solution here)
-
-/* return $response
-  ->withHeader('Location', 'https://www.example.com')
-  ->withStatus(302); */
-
-// BEGIN (write your solution here)
-
-$app->get('/companies/{id:[0-9]+}', function ($request, $response, $args) use ($companies) {
-    $id = $args['id'];
-    $company = collect($companies)->firstWhere('id', $id);
-    if (!$company) {
-        return $response->withStatus(404)->write('Page not found');
-    }
-    return $response->write(json_encode($company));
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id'], 'nickname' => 'user-' . $args['id']];
+    // Указанный путь считается относительно базовой директории для шаблонов, заданной на этапе конфигурации
+    // $this доступен внутри анонимной функции благодаря https://php.net/manual/ru/closure.bindto.php
+    // $this в Slim это контейнер зависимостей
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
 });
-
-// END
 
 $app->run();
