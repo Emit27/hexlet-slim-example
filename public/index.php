0abe79d23b80831e3ff0d5d750d1aadef7daa372
Dmitryi_Ignatev <46993682+Emit27@users.noreply.github.com>
2021-01-31 20:45:03 +1000

    searh form

--- public/index.php
+++ public/index.php
@@ -7,33 +7,30 @@ use DI\Container;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$users = \Src\Generator::generate(100);
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
 
 $container = new Container();
 $container->set('renderer', function () {
     return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
 });
-//print_r($users);
+
 AppFactory::setContainer($container);
 $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function ($request, $response) {
+/* $app->get('/users', function ($request, $response) {
     return $this->get('renderer')->render($response, 'index.phtml');
-});
+}); */
 
 // BEGIN (write your solution here)
-$app->get('/users[/{id:[0-9]+}]', function ($request, $response, $args) use ($users) {
-    if (!is_null($args['id'])) {
-        $id = (int) $args['id'];
-        $user = collect($users) -> firstWhere('id', $id);
-        $params2 = ['user' => collect($users) -> firstWhere('id', $id)];
-        return $this->get('renderer')->render($response, 'users/show.phtml', $params2);
-    } else {
-        $params = ['users' => $users];
+$app->get('/users', function ($request, $response) use ($users) {
+    $term = $request->getQueryParam('term');
+    $pos = array_filter($users, function ($user) use ($term) {
+        return is_int(strpos($user, $term));
+    });
+        $params = ['users' => $pos];
         return $this->get('renderer')->render($response, 'users/index.phtml', $params);
-    }
 });
 // END
 
-$app->run();
+    $app->run();
